f35413aac7946a5ac703137e45b5ae66627a2a3a
Jack Franklin <jack@jackfranklin.net>
2018-04-17 12:01:30 +0200

    exercise 13 and 14

---
+++ exercise14/Main.elm
@@ -0,0 +1,65 @@
+module Main exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (class, type_, value)
+import Html.Events exposing (onClick, onInput)
+
+
+type alias Model =
+    { count : Int
+    , userInput : String
+    }
+
+
+type Msg
+    = Increment
+    | NewUserIncrementInput String
+
+
+initialModel : Model
+initialModel =
+    { count = 0, userInput = "0" }
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Increment ->
+            -- EXERCISE: can you rewrite this code to use Result.withDefault?
+            -- so that we don't need to have the case statement any more.
+            case String.toInt model.userInput of
+                Ok number ->
+                    { model | count = model.count + number }
+
+                Err _ ->
+                    model
+
+        NewUserIncrementInput newValue ->
+            { model | userInput = newValue }
+
+
+view : Model -> Html Msg
+view model =
+    div
+        [ class "content" ]
+        [ h1 [] [ text "Amazing counter" ]
+        , div []
+            [ button [ onClick Increment ]
+                [ text "Increment" ]
+            , input
+                [ type_ "number"
+                , onInput NewUserIncrementInput
+                , value model.userInput
+                ]
+                []
+            , text (toString model.count)
+            ]
+        ]
+
+
+main =
+    Html.beginnerProgram
+        { model = initialModel
+        , view = view
+        , update = update
+        }
